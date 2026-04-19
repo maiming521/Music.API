ef2171a124b80570030ff25269c0f19a966b3826
maiming521 <8891660@qq.com>
2026-04-20 06:59:32 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,13 @@
-const { handler } = require('NeteaseCloudMusicApi');
+const api = require('NeteaseCloudMusicApi');
 
 module.exports = async (req, res) => {
-  // 强制JSON响应头！永久解决song/url浏览器二进制乱码
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  
-  try {
-    // 官方原生正确调用方式，再也不会报api/is not a function
-    await handler(req, res);
-  } catch (err) {
-    res.status(200).json({
-      code: -1,
-      msg: '接口异常',
-      error: err.message
-    });
-  }
+
+  // 调试：打印包里所有导出内容，看看到底是什么结构
+  res.status(200).json({
+    packageExportKeys: Object.keys(api),
+    apiIsFunction: typeof api === 'function',
+    handlerIsExist: !!api.handler,
+    defaultIsExist: !!api.default
+  });
 };
