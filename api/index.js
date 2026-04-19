888fd4928d1fd8a2c489da0668287bf9bc1b26a0
maiming521 <8891660@qq.com>
2026-04-20 07:05:44 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const api = require('NeteaseCloudMusicApi');
 
 module.exports = async (req, res) => {
-  // 强制JSON响应头！永久解决song/url浏览器二进制乱码问题
+  // 强制JSON响应头，彻底解决浏览器二进制乱码
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
 
   try {
-    // 官方原生正确调用方式！和你包里导出结构完全匹配
+    // 你包里唯一正确入口！刚才调试页面明确看到 serveNcmApi
     await api.serveNcmApi(req, res);
   } catch (err) {
     res.status(200).json({
