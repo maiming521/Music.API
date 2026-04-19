dfbbb8b8944a5be8b9d3289916427724183f9324
maiming521 <8891660@qq.com>
2026-04-20 07:13:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
-// 加载网易云API
-require('@binaryify/netease-cloud-music-api')(app);
+// 加载官方原版网易云API，和你之前调试包里结构100%匹配
+const api = require('NeteaseCloudMusicApi');
+api.serveNcmApi(app);
 
-// 全局修复 JSON 响应头，解决 song/url 乱码问题
+// 全局强制JSON响应头！永久解决song/url浏览器二进制乱码问题
 app.use((req, res, next) => {
   res.header('Content-Type', 'application/json; charset=utf-8');
   next();
